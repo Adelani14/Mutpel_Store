9bef67703dfde30d5c8669746e27abd38ca663a5
Adelani Ayofe <princeadelani27@gmail.com>
2026-07-08 07:10:02 -0700

    update

--- Client/src/components/MobileBottomNav.jsx
+++ Client/src/components/MobileBottomNav.jsx
@@ -44,8 +44,8 @@ function MobileBottomNav() {
 
                 <NavLink to="/cart" className={({ isActive }) =>
                     `nav-item ${isActive ? "active" : ""}`
-                } style={{ position: "relative", paddding: "0" }}>
-                    <i className="bi bi-cart4 fs-5"></i><span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">{cartCount}</span>
+                } style={{ position: "relative", paddding: "0", margin: "0"  }}>
+                    <i className="bi bi-cart4 fs-5"><span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">{cartCount}</span></i>
                     <small>Cart</small>
                 </NavLink>
 
