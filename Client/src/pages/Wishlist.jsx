03beae24a96a7a9d14f667bcb36ccebb0a977f11
Adelani Ayofe <princeadelani27@gmail.com>
2026-07-13 10:20:54 -0700

    update

--- Client/src/pages/Wishlist.jsx
+++ Client/src/pages/Wishlist.jsx
@@ -13,14 +13,14 @@ const Wishlist = () => {
 
 
     useEffect(() => {
-        fetchCart();
+        fetchWishlist();
     }, []);
 
-    const fetchCart = async () => {
+    const fetchWishlist = async () => {
         try {
-            const response = await Axios.get("/api/cart/getCart");
+            const response = await Axios.get("/api/wishlist/getWishlist");
 
-            setProducts(response.data.cart?.items || []);
+            setProducts(response.data.wishlist?.items || []);
 
 
 
@@ -35,55 +35,9 @@ const Wishlist = () => {
 
     }
 
-    const totalPrice = products.reduce((total, item) => {
-        if (!item.product) return total;
 
-        return total + item.product.price * item.quantity;
-    }, 0);
 
-    const increaseQuantity = async (item) => {
-        if (!item.product) return;
-        setLoading(true)
-        try {
-            await Axios.put("/api/cart/updateCartItem", {
-                productId: item.product._id,
-                quantity: item.quantity + 1,
-                size: item.size,
-                color: item.color,
-            });
-
-            await fetchCart();
-
-        } catch (error) {
-            console.log(error);
-        }
-        finally {
-            setLoading(false)
-        }
-    };
-
-    const decreaseQuantity = async (item) => {
-        if (!item.product) return;
-        setLoading(true)
-
-        try {
-            await Axios.put("/api/cart/updateCartItem", {
-                productId: item.product._id,
-                quantity: item.quantity - 1,
-                size: item.size,
-                color: item.color,
-            });
-
-            await fetchCart();
-        } catch (error) {
-            console.log(error);
-        }
-        finally {
-            setLoading(false)
-        }
-    };
-
-    const removeFromCart = async (item) => {
+    const removeFromWishlist = async (item) => {
         const isConfirmed = window.confirm(
             "Are you sure you want to remove this product?"
         );
@@ -93,14 +47,14 @@ const Wishlist = () => {
         try {
             if (!item.product) return;
 
-            await Axios.delete("/api/cart/removeFromCart", {
+            await Axios.delete("/api/wishlist/removeFromWishlist", {
                 data: {
                     productId: item.product._id,
                     size: item.size,
                     color: item.color,
                 },
             });
-            fetchCart(); // refresh cart
+            fetchWishlist(); // refresh wishlist
         } catch (error) {
             console.log(error);
 
@@ -108,15 +62,15 @@ const Wishlist = () => {
     };
 
 
-    const clearCart = async () => {
+    const clearWishlist = async () => {
         const isConfirmed = window.confirm(
-            "Are you sure you want to clear all Cart!?"
+            "Are you sure you want to clear all wishlist!?"
         );
 
         if (!isConfirmed) return;
         setLoading(true)
         try {
-            await Axios.delete("/api/cart/clearCart");
+            await Axios.delete("/api/wishlist/clearWishlist");
 
             setProducts([]);
         } catch (error) {
@@ -180,13 +134,13 @@ const Wishlist = () => {
                             }}
                         >
                             <i
-                                className="bi bi-cart-x text-primary"
+                                className="bi bi-heart-x text-primary"
                                 style={{ fontSize: "4rem" }}
                             ></i>
                         </div>
 
                         <h2 className="fw-bold">
-                            Your cart is empty
+                            Your wishlist is empty
                         </h2>
 
                         <p
@@ -249,55 +203,53 @@ const Wishlist = () => {
                                             className=" col-6 col-md-3 col-lg-2 card border-0 rounded-4 shadow-sm h-100"
                                         >
 
-                                            <div className="card border-0 rounded-4 shadow-sm h-100 overflow-hidden product-card">
+                                            <div className="position-relative">
 
-                                                <div className="position-relative">
+                                                <Link to={`/productdetail/${item.product._id}`}>
 
-                                                    <Link to={`/productdetail/${item.product._id}`}>
+                                                    <img src={item.product?.imagespath[0]?.url}
+                                                        className="card-img-top"
+                                                        style={{
+                                                            height: 220,
+                                                            objectFit: "cover",
+                                                        }} alt={item.product?.title} />
+                                                </Link>
 
-                                                        <img src={item.product?.imagespath[0]?.url}
-                                                            className="card-img-top"
-                                                            style={{
-                                                                height: 220,
-                                                                objectFit: "cover",
-                                                            }} alt={item.product?.title} />
-                                                    </Link>
+                                                {item.product?.discountPercentage > 0 && (
+                                                    <span className="badge bg-danger position-absolute top-0 start-0 m-2">
+                                                        -{item.product?.discountPercentage}%
+                                                    </span>
+                                                )}
 
-                                                    {item.product?.discountPercentage > 0 && (
-                                                        <span className="badge bg-danger position-absolute top-0 start-0 m-2">
-                                                            -{item.product?.discountPercentage}%
-                                                        </span>
-                                                    )}
-
-                                                    <button
-                                                        className="btn btn-light rounded-circle position-absolute top-0 end-0 m-2"
-                                                    >
-                                                        <i className="bi bi-heart"></i>
-                                                    </button>
-
-                                                </div>
-
-                                                <div className="card-body">
-
-                                                    <Link to={`/productdetail/${item.product._id}`} className="text-decoration-none text-dark">
-                                                        <h2 className="h5 mb-2">{item.product?.title}</h2>
-                                                    </Link>
-
-
-
-                                                    <div className="fw-bold fs-5">
-                                                        ₦{item.product?.price}
-                                                    </div>
-
-                                                    {item.product?.previousPrice > 0 && (
-                                                        <small className="text-decoration-line-through text-muted">
-                                                            ₦{item.product?.previousPrice}
-                                                        </small>
-                                                    )}
-
-                                                </div>
+                                                <button
+                                                    className="btn btn-light rounded-circle position-absolute top-0 end-0 m-2"
+                                                >
+                                                    <i className="bi bi-heart"></i>
+                                                </button>
 
                                             </div>
+
+                                            <div className="card-body">
+
+                                                <Link to={`/productdetail/${item.product._id}`} className="text-decoration-none text-dark">
+                                                    <h2 className="h5 mb-2">{item.product?.title}</h2>
+                                                </Link>
+
+
+
+                                                <div className="fw-bold fs-5">
+                                                    ₦{item.product?.price}
+                                                </div>
+
+                                                {item.product?.previousPrice > 0 && (
+                                                    <small className="text-decoration-line-through text-muted">
+                                                        ₦{item.product?.previousPrice}
+                                                    </small>
+                                                )}
+
+                                            </div>
+
+
                                         </div>
                                     ))}
 
