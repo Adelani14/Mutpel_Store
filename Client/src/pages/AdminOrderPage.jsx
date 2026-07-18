c82725e6484e89d6dbc6a0a9b8abcb6598ed3ac4
ABDULSEMIU SODEEQ ADELANI <princeadelani27@gmail.com>
2026-07-18 22:05:34 +0100

    Update AdminOrderPage.jsx

--- Client/src/pages/AdminOrderPage.jsx
+++ Client/src/pages/AdminOrderPage.jsx
@@ -1,7 +1,7 @@
 import Axios from '../utils/axiosInstance.js'
 import { useEffect, useMemo, useState } from "react";
 import { Link } from "react-router-dom";
-import Axios from "../utils/axiosInstance.js";
+
 
 
 const AdminOrderPage = () => {
