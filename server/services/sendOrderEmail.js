991e067a58980f06250030854d55cfecc7a8fb5a
Adelani Ayofe <princeadelani27@gmail.com>
2026-07-21 11:02:56 -0700

    update

--- server/services/sendOrderEmail.js
+++ server/services/sendOrderEmail.js
@@ -1,72 +1,75 @@
 import axios from "axios";
 
 const sendOrderEmail = async (order) => {
+    // const html = `
+    //     <div style="font-family:Arial,sans-serif;padding:20px">
+
+    //         <h2 style="color:#0d6efd">
+    //             Thank you for shopping with Mutpel Household
+    //         </h2>
+
+    //         <p>
+    //             Hello <strong>${order.shippingAddress.fullName}</strong>,
+    //         </p>
+
+    //         <p>
+    //             We've successfully received your payment and your order has been placed.
+    //         </p>
+
+    //         <hr>
+
+    //         <h3>Order Summary</h3>
+
+    //         <p>
+    //             <strong>Reference:</strong>
+    //             ${order.paymentReference}
+    //         </p>
+
+    //         <p>
+    //             <strong>Total:</strong>
+    //             ₦${order.totalAmount.toLocaleString()}
+    //         </p>
+
+    //         <p>
+    //             <strong>Payment:</strong>
+    //             Paid
+    //         </p>
+
+    //         <p>
+    //             <strong>Status:</strong>
+    //             ${order.orderStatus}
+    //         </p>
+
+    //         <hr>
+
+    //         <h3>Items</h3>
+
+    //         ${order.orderItems.map(item => `
+    //             <div style="margin-bottom:12px">
+    //                 <strong>${item.title}</strong><br>
+    //                 Qty: ${item.quantity}<br>
+    //                 ₦${item.subtotal.toLocaleString()}
+    //             </div>
+    //         `).join("")}
+
+    //         <hr>
+
+    //         <p>
+    //             We'll notify you again when your order is confirmed and when it has been shipped.
+    //         </p>
+
+    //         <br>
+
+    //         <strong>
+    //             Mutpel Household
+    //         </strong>
+
+    //     </div>
+    // `;
     const html = `
-        <div style="font-family:Arial,sans-serif;padding:20px">
-
-            <h2 style="color:#0d6efd">
-                Thank you for shopping with Mutpel Household
-            </h2>
-
-            <p>
-                Hello <strong>${order.shippingAddress.fullName}</strong>,
-            </p>
-
-            <p>
-                We've successfully received your payment and your order has been placed.
-            </p>
-
-            <hr>
-
-            <h3>Order Summary</h3>
-
-            <p>
-                <strong>Reference:</strong>
-                ${order.paymentReference}
-            </p>
-
-            <p>
-                <strong>Total:</strong>
-                ₦${order.totalAmount.toLocaleString()}
-            </p>
-
-            <p>
-                <strong>Payment:</strong>
-                Paid
-            </p>
-
-            <p>
-                <strong>Status:</strong>
-                ${order.orderStatus}
-            </p>
-
-            <hr>
-
-            <h3>Items</h3>
-
-            ${order.orderItems.map(item => `
-                <div style="margin-bottom:12px">
-                    <strong>${item.title}</strong><br>
-                    Qty: ${item.quantity}<br>
-                    ₦${item.subtotal.toLocaleString()}
-                </div>
-            `).join("")}
-
-            <hr>
-
-            <p>
-                We'll notify you again when your order is confirmed and when it has been shipped.
-            </p>
-
-            <br>
-
-            <strong>
-                Mutpel Household
-            </strong>
-
-        </div>
-    `;
-
+<h1>Hello from Brevo!</h1>
+<p>This is a test email.</p>
+`;
     try {
 
         const response = await axios.post(
